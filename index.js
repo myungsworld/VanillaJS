2d56062ef6ba2f90d9145d39f620cf26baf64847
myungsworld <myungsworld@gmail.com>
2020-06-27 16:55:18 +0900

    4nd lec updated

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-// const what = "song";
+// const monday = 'Mon';
+// const tue = 'Tue';
+// const wed = 'Wed';
+// const thu = 'Thu';
+// const fri = 'Fri';
 
-// console.log(what);
-/*
-const wat = true;
+// console.log(monday, tue,wed,thu,fri);
 
-console.log(wat);
-*/
+// const daysOfWeek = [
+//     'Mon','Tue','Wed',
+//     'Thu','Fri','Sat',
+//     'Sun','Sun',54,true];
+// console.log(daysOfWeek);
 
-const wat = 55.1;
-console.log(wat);
+const songInfo = {
+    name:"song",
+    age:"27",
+    gender:"Male",
+    isHandsome:true,
+    favMovies:["Road of rings","HarryPotter","IronMan"],
+    favFood:[
+    {
+        name: "burger",
+        fatty:true
+
+    },
+    {
+        name: "min",
+        fartty:false
+    }
+    ]
+}
+
+console.log(songInfo);
+console.log(songInfo.age);
+console.log(songInfo.favFood[1].name);
