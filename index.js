2df8a011b4a5615ba9752ab311fafc8abff99eab
myungsworld <myungsworld@gmail.com>
2020-06-28 05:05:30 +0900

    6th lec updated

--- index.js
+++ index.js
@@ -31,14 +31,36 @@
 //     ]
 // }
 
-// console.log(songInfo);
+// console.log(songInfo); 
 // console.log(songInfo.age);
 // console.log(songInfo.favFood[1].name);
 
 function sayHello(name,age) {
     for (let i = 0; i<3 ; i++) {
-        console.log('hello',name,"your age",age)
+        console.log(`Hello ${name} you are ${age} years old `)
     }
+    return 0;
 }
 
-sayHello("song",15)
+const greetSong = sayHello("song",27);
+
+console.log(greetSong);
+
+const calculator = {
+    plus: function(a,b) {
+        return a + b;
+    },
+    minus: function(a,b) {
+        return a - b;
+    },
+    multiple: function(a,b) {
+        return a * b;
+    },
+    divide: function(a,b) {
+        return a/b;
+    }
+}
+console.log(calculator.plus(5,5))
+console.log(calculator.minus(5,5))
+console.log(calculator.multiple(5,5))
+console.log(calculator.divide(5,5))
