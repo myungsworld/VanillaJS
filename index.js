07cbb7c062d2e652426d33a7cc53145a44d7148d
myungsworld <myungsworld@gmail.com>
2020-06-28 17:43:40 +0900

    7th lec updated

--- index.js
+++ index.js
@@ -35,32 +35,37 @@
 // console.log(songInfo.age);
 // console.log(songInfo.favFood[1].name);
 
-function sayHello(name,age) {
-    for (let i = 0; i<3 ; i++) {
-        console.log(`Hello ${name} you are ${age} years old `)
-    }
-    return 0;
-}
+// function sayHello(name,age) {
+//     for (let i = 0; i<3 ; i++) {
+//         console.log(`Hello ${name} you are ${age} years old `)
+//     }
+//     return 0;
+// }
 
-const greetSong = sayHello("song",27);
+// const greetSong = sayHello("song",27);
 
-console.log(greetSong);
+// console.log(greetSong);
 
-const calculator = {
-    plus: function(a,b) {
-        return a + b;
-    },
-    minus: function(a,b) {
-        return a - b;
-    },
-    multiple: function(a,b) {
-        return a * b;
-    },
-    divide: function(a,b) {
-        return a/b;
-    }
-}
-console.log(calculator.plus(5,5))
-console.log(calculator.minus(5,5))
-console.log(calculator.multiple(5,5))
-console.log(calculator.divide(5,5))
+// const calculator = {
+//     plus: function(a,b) {
+//         return a + b;
+//     },
+//     minus: function(a,b) {
+//         return a - b;
+//     },
+//     multiple: function(a,b) {
+//         return a * b;
+//     },
+//     divide: function(a,b) {
+//         return a/b;
+//     }
+// }
+// console.log(calculator.plus(5,5))
+// console.log(calculator.minus(5,5))
+// console.log(calculator.multiple(5,5))
+// console.log(calculator.divide(5,5))
+ 
+const title = document.querySelector('#title');
+title.innerHTML = "Hi! From JS";
+title.style.color = 'red'; 
+document.title = "i owe you"; 
